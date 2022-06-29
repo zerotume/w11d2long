e9d9f0ff09754432da1db0f05c9cb8c109d365fc
zerotume <zerotume@gmail.com>
2022-06-29 14:15:56 -0700

    added user input validation on user login backend endpoint

--- authenticate-me/backend/routes/api/session.js
+++ authenticate-me/backend/routes/api/session.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const {check} = require('express-validator');
+const {handleValidationErrors} = require('../../utils/validation.js');
 
 const {setTokenCookie, restoreUser} = require('../../utils/auth.js');
 const {User} = require('../../db/models');
 
 const router = express.Router();
+
 
 router.get('/', restoreUser, (req, res) => {
     const {user} = req;
@@ -14,7 +17,18 @@ router.get('/', restoreUser, (req, res) => {
     }
 });
 
-router.post('/', async (req,res,next) => {
+const validateLogin = [
+    check('credential')
+        .exists({checkFalsy: true})
+        .notEmpty()
+        .withMessage('Please provide a valid email or username'),
+    check('password')
+        .exists({checkFalsy: true})
+        .withMessage('Please provide a password'),
+    handleValidationErrors
+]
+
+router.post('/', validateLogin, async (req,res,next) => {
     const {credential, password} = req.body;
 
     const user = await User.login({credential, password});
