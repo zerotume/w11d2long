e71e4fbed022833f0c473864e3d760c6f9a8b196
zerotume <zerotume@gmail.com>
2022-06-29 13:56:12 -0700

    added user login,logout,signup

--- authenticate-me/backend/routes/api/index.js
+++ authenticate-me/backend/routes/api/index.js
@@ -1,11 +1,18 @@
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+const {restoreUser} = require('../../utils/auth.js');
+router.use(restoreUser);
+router.use('/session', sessionRouter);
+router.use('/users', usersRouter);
+router.post('/test', (req,res) => {
+    res.json({requestBody:req.body});
+});
 
 // router.post('/test', (req,res) => {
 //     res.json({requestBody:req.body});
 // });
 
-const {restoreUser} = require('../../utils/auth.js');
-router.use(restoreUser);
 
 // router.get('/restore-user', (req, res) => {
 //     return res.json(req.user);
